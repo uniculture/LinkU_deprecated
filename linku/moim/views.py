189e904984f5fdad0fd623078d67770554f2a30f
jang <wkdtjsgur100@naver.com>
2017-02-19 21:38:19 +0900

    Use meeting_list.html in moim/views.py

--- linku/moim/views.py
+++ linku/moim/views.py
@@ -1,9 +1,8 @@
 from django.http import HttpResponse
 from moim.models import Meeting
+from django.shortcuts import render
 
 
 def homepage(request):
-    content = ''
-    for obj in Meeting.objects.all():
-        content += obj.name + "\n"
-    return HttpResponse(content)
+    meetings = Meeting.objects.all()
+    return render(request, 'meeting_list.html', {'meetings': meetings})
